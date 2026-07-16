354ebf28b51303050a74534f59466813b41ffc08
notmc1 <304993068+notmc1@users.noreply.github.com>
2026-07-16 23:47:04 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# atctcas24
+# tcas24
 A basic ATC24 TCAS system
+# What is tcas24?
+TCAS24 is a TCAS (traffic alert and collision avoidance system) for ATC24 that uses your distance, altitude and direction from other planes in ATC24 and issues RA (Resolution advisories) in case it is needed.
+# How do I use tcas24? 
+Simply go to the website https://tcas24.onrender.com and click Connect.
+# How do I report a bug/suggestion?
+Please create an issue. If you have a suggestion to the code, feel free to create a pull request.
+# I have a question about tcas24
+You can ask your question in Issues or message me. I will try to respond as soon as possible.
+# I hope you enjoy using tcas24!
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require("path");
+const WebSocket = require("ws");
+
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const server = app.listen(process.env.PORT || 8080, () => {
+    console.log("Server running on port", process.env.PORT || 8080);
+});
+
+const relay = new WebSocket.Server({ server });
+
+
+const ptfs = new WebSocket("wss://24data.ptfs.app/wss", {
+    headers: {}
+});
+
+ptfs.on("open", () => {
+    console.log("Server websocket connection attempt made to 24data");
+});
+
+ptfs.on("error", (err) => {
+    console.error("Oops, restart. Error:", err);
+});
+
+ptfs.on("message", (data) => {
+    const text = data.toString();
+
+    relay.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(text);
+        }
+    });
+});
+
+relay.on("connection", () => {
+    console.log("Server websocket connection established.");
+});
